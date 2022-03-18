e02d61c115e90d4a23520d3d8324395d104c9e31
Mitch Richling <11151403+richmit@users.noreply.github.com>
2022-03-17 20:54:20 -0500

    Added background dialog

--- Color_Accumulator_Empty.js
+++ Color_Accumulator_Empty.js
@@ -4,6 +4,8 @@
 // This script simply creates a new ColorAccumulator image of the same type and size as the current slice.
 //
 // Useful to initialize a sequence of ColorAccumulator operations if you want to start with an accumulator with a specific background color for example.
+//
+// Supports 8-bit integer grey, 16-bit integer grey, 32-bit float grey, & 24-bit RGB
 
 function main() {
 
@@ -12,7 +14,7 @@ function main() {
     return false;
   }
 
-  var accImg    = Packages.ij.WindowManager.getImage("ColorAccumulator");
+  var accImg = Packages.ij.WindowManager.getImage("ColorAccumulator");
   if (accImg) {
 	Packages.ij.IJ.showMessage("ERROR(Color_Accumulator_Empty.js): ColorAccumulator image already exists!");
     return false;
@@ -20,10 +22,46 @@ function main() {
 
   var srcImg = Packages.ij.IJ.getImage();
   var srcPro = srcImg.getProcessor();
+
+  var dialogObj = new Packages.ij.gui.GenericDialog("Color Accumulator Empty");
+
+  if ( (srcImg.getBitDepth() == 8) || (srcImg.getBitDepth() == 16)) {
+    dialogObj.addNumericField("Color: ",   0, 0, 5, "");
+  } else if (srcImg.getBitDepth() == 32) {
+    dialogObj.addNumericField("Color: ",   0, 3, 8, "");
+  } else if (srcImg.getBitDepth() == 24) {
+    dialogObj.addChoice("Color: ", ["White", "Black", "Red", "Green", "Blue", "Yellow", "Cyan", "Magenta"], "Green");
+  } else {
+    Packages.ij.IJ.showMessage("ERROR(Color_Accumulator_Empty.js): Image depth of " + srcImg.getBitDepth() + " is unsupported!  Must be 8, 16, or 24");
+    return false;
+  }
+
+  dialogObj.showDialog();
+
+  if (dialogObj.wasCanceled())
+    return;
+
+  if ( (srcImg.getBitDepth() == 8) || (srcImg.getBitDepth() == 16)) {
+    var bgColor = Math.abs(Math.round(dialogObj.getNextNumber()));
+  } else if (srcImg.getBitDepth() == 32) {
+    var bgColor = dialogObj.getNextNumber();
+  } else if (srcImg.getBitDepth() == 24) {
+    switch(dialogObj.getNextChoice()) {
+      case "White"   : var bgColor = 0xFFFFFF; break;
+      case "Black"   : var bgColor = 0x000000; break;
+      case "Red"     : var bgColor = 0xFF0000; break;
+      case "Green"   : var bgColor = 0x00FF00; break;
+      case "Blue"    : var bgColor = 0x0000FF; break;
+      case "Yellow"  : var bgColor = 0xFFFF00; break;
+      case "Cyan"    : var bgColor = 0x00FFFF; break;
+      case "Magenta" : var bgColor = 0xFF00FF; break;
+    }
+  } 
+
   var accPro = srcPro.duplicate();
   var accPix = accPro.getPixels();
   for (var i=0; i<accPix.length; i++)
-    accPix[i] = 0;
+    accPix[i] = bgColor;
 
   accImg = new Packages.ij.ImagePlus("ColorAccumulator", accPro);
   accImg.show();
