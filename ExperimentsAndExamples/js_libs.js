df1a3503bf547b6ea00536347f925b3c31152833
Mitch Richling <11151403+richmit@users.noreply.github.com>
2022-03-18 23:20:26 -0500

    Added lib example

---
+++ ExperimentsAndExamples/js_libs.js
@@ -0,0 +1,7 @@
+
+// One way to load a js library.
+load(Packages.ij.IJ.getDirectory("plugins") + "MJR_LIB_js" + java.io.File.separator + "some-library.js");
+
+// Feels klunky.  
+// The directory shows up in the menus too.
+
